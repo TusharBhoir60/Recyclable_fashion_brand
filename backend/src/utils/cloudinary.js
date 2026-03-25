7829523b077cd86bdb91b8fe8bb7d789cadaa27f
Tushar <tusharbhoir6969@gmail.com>
2026-03-25 22:41:52 +0530

    letest push

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,1 +1,28 @@
-module.exports = require('./claudinary');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+/**
+ * Upload a Buffer to Cloudinary.
+ * @param {Buffer} buffer
+ * @param {string} folder
+ * @returns {Promise<string>} secure_url
+ */
+async function uploadBuffer(buffer, folder = 'recyclabag') {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder, resource_type: 'image' },
+      (err, result) => {
+        if (err) return reject(err);
+        resolve(result.secure_url);
+      }
+    );
+    stream.end(buffer);
+  });
+}
+
+module.exports = { cloudinary, uploadBuffer };
